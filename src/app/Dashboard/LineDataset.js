fa7c1b4abdc6494ebcb5f923844e79cb0214547e
unclethien <dxn210021@utdallas.edu>
2024-11-16 22:12:25 -0600

    update

--- src/app/Dashboard/LineDataset.js
+++ src/app/Dashboard/LineDataset.js
@@ -1,69 +1,107 @@
 "use client";
+import * as React from "react";
+import { LineChart } from "@mui/x-charts/LineChart";
+import utilityUsage from "../data/utility_usage.json";
 
-import * as React from 'react';
-import { LineChart } from '@mui/x-charts/LineChart';
-import utilityUsage from '../data/utility_usage.json'; 
+const dataset = utilityUsage.floors.flatMap((floor) =>
+  floor.hourlyData.map((data) => ({
+    hour: data.hour,
+    electricity: data.electricity,
+    water: data.water,
+    floor: `Floor ${floor.floor}`,
+  }))
+);
 
-const stackStrategy = {
-  stack: 'total',
-  area: true,
-  stackOffset: 'none', // To stack 0 on top of others
-};
-
-const customize = {
-  width: 700,
-  height: 300,
-  legend: { hidden: true },
-  margin: { top: 5 },
-};
-
-// Flattening and formatting the data for the chart
-
-
-export default function LineDataset() {
-  
-
-  const formatDataForChart = (data) => {
-    return data.floors.flatMap(floor => {
-      return floor.hourlyData.map((entry) => ({
-        hour: entry.hour,
-        electricity: entry.electricity,
-        water: entry.water,
-        floor: floor.floor,
-      }));
-    });
-  };
-
-  const formattedData = formatDataForChart(utilityUsage); // Get the formatted data
-
+export default function StackedAreas() {
   return (
-    <LineChart
-      xAxis={[
-        {
-          dataKey: 'hour',
-          valueFormatter: (value) => value.toString(),
-          min: 0,
-          max: 23,
-        },
-      ]}
-      series={[
-        {
-          dataKey: 'electricity',
-          label: 'Electricity',
-          color: 'blue', // Choose an appropriate color
-          showMark: false,
-          ...stackStrategy,
-        },
-        {
-          dataKey: 'water',
-          label: 'Water',
-          color: 'green', // Choose an appropriate color
-          showMark: false,
-          ...stackStrategy,
-        },
-      ]}
-      dataset={formattedData}
-      {...customize}
-    />
+    <div>
+      <h2>Electricity Usage</h2>
+      <LineChart
+        dataset={dataset}
+        xAxis={[
+          {
+            id: "Hours",
+            dataKey: "hour",
+            scaleType: "linear",
+          },
+        ]}
+        series={[
+          {
+            id: "Floor 1 Electricity",
+            label: "Floor 1 Electricity",
+            dataKey: "electricity",
+            stack: "total",
+            area: true,
+            showMark: false,
+            filter: (data) => data.floor === "Floor 1",
+          },
+          {
+            id: "Floor 2 Electricity",
+            label: "Floor 2 Electricity",
+            dataKey: "electricity",
+            stack: "total",
+            area: true,
+            showMark: false,
+            filter: (data) => data.floor === "Floor 2",
+          },
+          {
+            id: "Floor 3 Electricity",
+            label: "Floor 3 Electricity",
+            dataKey: "electricity",
+            stack: "total",
+            area: true,
+            showMark: false,
+            filter: (data) => data.floor === "Floor 3",
+          },
+        ]}
+        width={600}
+        height={400}
+        margin={{ left: 70 }}
+      />
+
+      <h2>Water Usage</h2>
+      <LineChart
+        dataset={dataset}
+        xAxis={[
+          {
+            id: "Hours",
+            dataKey: "hour",
+            scaleType: "linear",
+          },
+        ]}
+        series={[
+          {
+            id: "Floor 1 Water",
+            label: "Floor 1 Water",
+            dataKey: "water",
+            stack: "total",
+            area: true,
+            showMark: false,
+            filter: (data) => data.floor === "Floor 1",
+          },
+          {
+            id: "Floor 2 Water",
+            label: "Floor 2 Water",
+            dataKey: "water",
+            stack: "total",
+            area: true,
+            showMark: false,
+            filter: (data) => data.floor === "Floor 2",
+          },
+          {
+            id: "Floor 3 Water",
+            label: "Floor 3 Water",
+            dataKey: "water",
+            stack: "total",
+            area: true,
+            showMark: false,
+            filter: (data) => data.floor === "Floor 3",
+          },
+        ]}
+        width={600}
+        height={400}
+        margin={{ left: 70 }}
+      />
+    </div>
   );
 }
