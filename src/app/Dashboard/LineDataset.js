c42a3b24759ee2e6aa8823a7ee8339d08264d646
adamabsa123 <adamabsa@gmail.com>
2024-11-16 21:08:32 -0600

    Update

--- src/app/Dashboard/LineDataset.js
+++ src/app/Dashboard/LineDataset.js
@@ -1,10 +1,8 @@
+"use client";
+
 import * as React from 'react';
 import { LineChart } from '@mui/x-charts/LineChart';
-import {
-  worldElectricityProduction,
-  keyToLabel,
-  colors,
-} from './LineDataset';
+import utilityUsage from '../data/utility_usage.json'; 
 
 const stackStrategy = {
   stack: 'total',
@@ -13,30 +11,58 @@ const stackStrategy = {
 };
 
 const customize = {
+  width: 700,
   height: 300,
   legend: { hidden: true },
   margin: { top: 5 },
 };
 
+// Flattening and formatting the data for the chart
+
+
 export default function LineDataset() {
+  
+
+  const formatDataForChart = (data) => {
+    return data.floors.flatMap(floor => {
+      return floor.hourlyData.map((entry) => ({
+        hour: entry.hour,
+        electricity: entry.electricity,
+        water: entry.water,
+        floor: floor.floor,
+      }));
+    });
+  };
+
+  const formattedData = formatDataForChart(utilityUsage); // Get the formatted data
+
   return (
     <LineChart
       xAxis={[
         {
-          dataKey: 'year',
+          dataKey: 'hour',
           valueFormatter: (value) => value.toString(),
-          min: 1985,
-          max: 2022,
+          min: 0,
+          max: 23,
         },
       ]}
-      series={Object.keys(keyToLabel).map((key) => ({
-        dataKey: key,
-        label: keyToLabel[key],
-        color: colors[key],
-        showMark: false,
-        ...stackStrategy,
-      }))}
-      dataset={worldElectricityProduction}
+      series={[
+        {
+          dataKey: 'electricity',
+          label: 'Electricity',
+          color: 'blue', // Choose an appropriate color
+          showMark: false,
+          ...stackStrategy,
+        },
+        {
+          dataKey: 'water',
+          label: 'Water',
+          color: 'green', // Choose an appropriate color
+          showMark: false,
+          ...stackStrategy,
+        },
+      ]}
+      dataset={formattedData}
       {...customize}
     />
   );
